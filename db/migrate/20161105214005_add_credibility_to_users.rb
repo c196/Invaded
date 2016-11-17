da39501c89810343ef589190e43fe94687983320
c196 <luca.a.mueller@gmail.com>
2016-11-17 14:58:05 +0100

    differentFixes

--- db/migrate/20161105214005_add_credibility_to_users.rb
+++ db/migrate/20161105214005_add_credibility_to_users.rb
@@ -1,6 +1,6 @@
 class AddCredibilityToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :credibility, :integer
-    add_column :users, :score, :integer
+    add_column :users, :credibility, :integer, :default => 500
+    add_column :users, :score, :integer, :default => 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,12 +32,12 @@ ActiveRecord::Schema.define(version: 20161105214005) do
 
   create_table "users", force: :cascade do |t|
     t.string   "user_name",              limit: 255
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "",  null: false
+    t.string   "encrypted_password",     limit: 255, default: "",  null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,   null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
@@ -46,10 +46,10 @@ ActiveRecord::Schema.define(version: 20161105214005) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email",      limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.integer  "credibility",            limit: 4
-    t.integer  "score",                  limit: 4
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+    t.integer  "credibility",            limit: 4,   default: 500
+    t.integer  "score",                  limit: 4,   default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
